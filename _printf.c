dc069145451ad9724d008969222c8a3f297ce12a
amiroudiallo.yw <80524359+amiroudiallo-dev@users.noreply.github.com>
2023-10-16 00:27:53 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,10 +1,10 @@
 /*
  * File: _printf.c
- * Auth: Brennan D Baraban
- *       Michael Klein
+ * Auth: DIALLO Amirou Y W
+ *       Ahmed Salama
  */
 
-#include "holberton.h"
+#include "main.h"
 
 void cleanup(va_list args, buffer_t *output);
 int run_printf(const char *format, va_list args, buffer_t *output);
