23c8e14628e7d652d2cfadd0291db54496dc3774
ahmedmohamed2210 <halawaahmed507@gmail.com>
2023-10-17 02:59:02 +0300

    m

--- main.h
+++ main.h
@@ -1,15 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>  
+#include <stdio.h>
+#include <unistd.h>
 
-
-#include <stdarg.h>  // For handling variadic arguments
-#include <stdio.h>   // For standard input/output
+#define BUFF_SIZE 1024
 
 int print_integer(int value);
 void print_buffer(char buffer[], int *buff_ind);
-
-
-#define BUFF_SIZE 1024
 
 #endif
